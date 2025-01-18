0df52cb3989d99c29a078f3a64b7c322dccf6dec
Abdul Rafi <63101627+TakeMeGH@users.noreply.github.com>
2025-01-18 23:37:31 +0700

    Input, state machine, yarn spinner

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace TKM
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Scriptable Objects/InputReader")]
+    public class InputReader : ScriptableObject, MCInput.IGameplayActions, MCInput.IUIActions
+    {
+        public Action<Vector2> MoveEvent;
+
+        MCInput _MCInput;
+        void OnEnable()
+        {
+            if (_MCInput == null)
+            {
+                _MCInput = new MCInput();
+                _MCInput.Gameplay.SetCallbacks(this);
+                _MCInput.UI.SetCallbacks(this);
+            }
+
+            EnableGameplayInput();
+        }
+
+        void OnDisable()
+        {
+            if (_MCInput != null) _MCInput.Gameplay.Disable();
+        }
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            MoveEvent?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void EnableGameplayInput()
+        {
+            _MCInput.Gameplay.Enable();
+            _MCInput.UI.Disable();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        public void EnableUIInput()
+        {
+            _MCInput.Gameplay.Disable();
+            _MCInput.UI.Enable();
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+    }
+}
